1a9d2655256bae97a46d28211becbda9b9a04db4
Hannes <luidale@gmail.com>
2019-01-14 22:34:04 +0200

    updated

--- README.md
+++ README.md
@@ -8,15 +8,17 @@ Example/Näide:
 
 Creates Estonian county countour map with:
 * two lakes (Peipsi and Võrtsjärv),
-* capital (Tallinn) 
-* and some counties labeled.
+* some towns (Tallinn and Tartu),
+* some counties labeled,
+* and neighbouring countries and Baltic Sea.
 
 Map will be saved as PNG and PDF.
 ***
 Teeb Eesti maakondade kontuurkaardi koos:
 * kahe järve (Peipsi järv ja Võrtsjärv),
-* pealinnaga (Tallinn)
-* mõned maakonnad on eraldi märgitud.
+* mõne linnaga (Tallinn, Tartu),
+* mõned maakonnad on eraldi märgitud
+* ja naaber riigid ning Läänemeri on eraldi märgitud.
 
 Kaart salvestatakse PNG ja PDF-na.
 ***
@@ -26,7 +28,6 @@ Usage:
 * Run script
 ***
 Kasutamine:
-Usage:
 * Ava fail "estonia_county_contour.R" R-s või RStudio-s
 * Muuda koodi
 * Jooksuta koodi
--- estonia_county_contour.R
+++ estonia_county_contour.R
@@ -32,6 +32,7 @@ output_file = "estonia_county_contour"
 est0 <- getData('GADM', country='EST', level=0)
 est1 <- getData('GADM', country='EST', level=1)
 est2 <- getData('GADM', country='EST', level=2)
+est3 <- getData('GADM', country='EST', level=3)
 lat0 <- getData('GADM', country='LVA', level=0)
 rus0 <- getData('GADM', country='RUS', level=0)
 rus1 <- getData('GADM', country='RUS', level=1)
@@ -53,7 +54,9 @@ plot(est1,add=TRUE, col="grey97")
 plot(est1[est1$NAME_1 %in% counties,], add=TRUE, col="grey")
 
 # Plot Tallinn with red
-plot(est2[20,], add=TRUE, col="red")
+plot(est2[est2$NAME_2 == "Tallinn",], add=TRUE, col="red")
+# Plot Tartu with red
+plot(est3[est3$NAME_3 == "Tartu",], add=TRUE, col="red")
 
 # Plot lakes with ligth blue
 plot(est1[est1$NAME_1 %in% c("Peipsi"),], add=TRUE, col="skyblue")
@@ -65,18 +68,25 @@ plot(rus2[(rus2$ENGTYPE_2 %in% c("Water body") & rus2$NAME_1 == "Pskov"),],
 # Add labels
 ## Selected counties
 label_size = 4
+label_size2 = 3
 for (i in 1:length(counties)) {
   text(coordinates(est1[est1$NAME_1 == counties[i],]),
        labels = paste(counties[i],"",sep="") #
-       ,cex=label_size)
+       ,cex=label_size2)
 }  
 ## Tallinn
-text(coordinates(est2[20,])+c(0.45,0), labels=c("Tallinn"), cex=label_size)
+text(coordinates(est2[20,])+c(0.5,0), labels=c("Tallinn"), cex=label_size)
+## Tartu
+text(coordinates(est2[179,])+c(0.15,0), labels=c("Tartu"), cex=label_size)
 ## Latvia
 text(coordinates(lat0)+c(0.45,0.7), labels=c("Latvia"), cex=label_size)
 ## Russia
 text(coordinates(lat0)+c(3.2,1.6), labels=c("Russia"), cex=label_size, srt=90)
-
+## Baltic Sea
+#text(coordinates(lat0)+c(-3.2,1.6), labels=c("Baltic Sea"), cex=label_size, srt=90)
+text(coordinates(lat0)+c(-3.2,2.6), labels=c("Baltic Sea"), cex=label_size, srt=90)
+#text(coordinates(lat0)+c(-3.2,3.6), labels=c("Baltic Sea"), cex=label_size, srt=90)
+#text(coordinates(lat0)+c(-3.2,4.6), labels=c("Baltic Sea"), cex=label_size, srt=90)
 # Save as PDF
 saveToPDF <- function(...) {
   d = dev.copy(pdf,...)
